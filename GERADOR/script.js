768005163a8a700250e25515161d9c93a0dac4a8
VALFREDO FELIPE DE SOUZA BRITO <125493023+ValfredoFelipe@users.noreply.github.com>
2023-03-22 08:20:17 -0300

    ,.
    .

--- GERADOR/script.js
+++ GERADOR/script.js
@@ -1,1 +1,29 @@
+let slider = document.querySelector('#slider')
+let buttonElement = document.querySelector('#butto')
+let sizePassword = document.querySelector('#valor')
+let password = document.querySelector('#password')
+let containerPassword = document.querySelector('#container-password')
+let charset = "abcdefghijlmnopqrstuvxzABCDEFGHIJLMNOPQRSTUVXZ1234567890"
+let novasenha = ""
+
+sizePassword.innerHTML = slider.value
+slider.oninput =function (){
+    sizePassword.innerHTML =this.value
+}
+function generatePassword(){
+    let pass = ""
+
+    for(let i = 0, n = charset.length; i< slider.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random()*n))
+    }
+    
+    containerPassword.classList.remove("hide")
+    password.innerHTML = pass
+}
+function copypassword(){
+    alert('Senha copiada com sucesso')
+    navigator.clipboard.writeText(novasenha)
+}
+
+     
 
---
+++ testando eventos/script.js
@@ -0,0 +1,81 @@
+/*window.onload = function(){
+
+    console.log('carregou o dom')
+    let title = document.querySelector('#title')
+    console.log(title)
+}-------------------------------------------------*/
+/*var clica = document.querySelector("#clica")
+
+console.log(clica)
+
+clica.addEventListener("click", function(){
+    console.log("clicou")
+    this.style.color = "red"
+})
+
+var title = document.querySelector('#title')
+
+title.addEventListener("click", function(){
+
+    var subtitle = document.querySelector("#subtitle")
+
+    subtitle.style.display = "none"
+
+})
+
+var subtitle = document.querySelector('#subtitle')
+
+subtitle.addEventListener("dblclick", function(){
+    console.log("click duplo")
+})
+---------------------------------------------------*/
+
+/*var title = document.querySelector("#title")
+
+title.addEventListener("mouseover",function(){
+
+    title.style.backgroundColor ="yellow"
+})
+
+var title = document.querySelector("#title")
+
+title.addEventListener("mouseout",function(){
+
+    title.style.backgroundColor ="white"
+})
+
+
+var subtitle = document.querySelector("#subtitle")
+
+subtitle.addEventListener("mouseover", function(){
+
+    var hide = document.querySelector("#legenda")
+
+    hide.classList.remove("hide" )
+})
+ 
+var subtitle = document.querySelector("#subtitle")
+
+subtitle.addEventListener("mouseout", function(){
+
+    var hide = document.querySelector("#legenda")
+
+    hide.classList.add("hide" )
+})------------------------------------------------*/
+/*document.addEventListener("keydown", function(event){
+
+    if(event.key ==="Enter") {
+        console.log("Apertou Enter")
+    }
+
+})
+document.addEventListener("keyup", function(event){
+
+    if(event.key ==="Enter") {
+        console.log("Soltou Enter")
+    }
+
+})*/
+
+
+
