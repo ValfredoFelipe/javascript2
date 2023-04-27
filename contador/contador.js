82aa637e8b8426be3c6bfca9000685161f0921cd
Valfredo Felipe de Souza Brito <valfredo155@hotmail.com>
2023-04-27 09:49:40 -0300

    .
    .

---
+++ contador/contador.js
@@ -0,0 +1,21 @@
+let count =0
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach((btn)=> {
+    
+    btn.addEventListener("click",(e)=>{
+        console.log(e.currentTarget)
+        // const styles =e.currentTarget.classList
+        // if(styles.contains("diminuir")){
+        //     count--
+        //     value.textContent=count
+        // }else if(styles.contains("aumentar")){
+        //     count++
+        //     value.textContent=count
+        //  } else {
+        //     count=0
+        // value.textContent=0
+        // }
+ })
+});
