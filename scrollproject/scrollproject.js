6d09d77948b242d48d81b652c28389dc29cff006
Valfredo Felipe de Souza Brito <valfredo155@hotmail.com>
2023-05-04 14:08:42 -0300

    .
    .

--- scrollproject/scrollproject.js
+++ scrollproject/scrollproject.js
@@ -8,5 +8,35 @@ const linksContainer= document.querySelector('.links-container')
 const links= document.querySelector('.links')
 
 navToggle.addEventListener("click", ()=>{
-    linksContainer.classList.toggle('show-links')
+    // linksContainer.classList.toggle('show-links')
+    const containerHeight=linksContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
+    if(containerHeight===0){
+        linksContainer.style.height =`${linksHeight}px`
+    }else{
+        linksContainer.style.height = 0
+    }
 })
+const navbar =document.getElementById('nav')
+const topLink =document.querySelector('.top-link')
+const linkSubir=document.querySelector('.clique-subir')
+// ********fixed navbar *************
+window.addEventListener("scroll", ()=>{
+    const scrollHeight = window.pageYOffset
+    const navHeight = navbar.getBoundingClientRect().height
+    if(scrollHeight>navHeight){
+        navbar.classList.add('fixed-nav')
+        
+    }else{
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if(scrollHeight>300){
+        linkSubir.classList.add('link-subir')
+    }else{
+        linkSubir.classList.remove('link-subir')
+    }
+    console.log(navHeight)
+
+})
+
