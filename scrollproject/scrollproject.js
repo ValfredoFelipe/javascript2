63869587c256c5bc23b21bf13c07395fc472b20e
Valfredo Felipe de Souza Brito <valfredo155@hotmail.com>
2023-05-08 11:02:21 -0300

    .
    .

--- scrollproject/scrollproject.js
+++ scrollproject/scrollproject.js
@@ -39,4 +39,31 @@ window.addEventListener("scroll", ()=>{
     console.log(navHeight)
 
 })
+// **********smoth scroll ************
+const scrollinks =document.querySelectorAll('.scroll-link')
+scrollinks.forEach((link)=>{
+    link.addEventListener('click',(e)=>{
+        e.preventDefault()
+        const id=e.currentTarget.getAttribute('href').slice(1)
+       
+        const element=document.getElementById(id)
+        // Calculando altura
+        const navHeight=navbar.getBoundingClientRect().height
+        const containerHeight =linksContainer.getBoundingClientRect().height
+        const fixedNav=navbar.classList.contains('fixed-nav')
 
+        let position=element.offsetTop - navHeight
+        
+        if(!fixedNav){
+            position =position -navHeight
+        }
+        if (navHeight>82){
+            position=position+containerHeight
+        }
+        window.scrollTo({
+        left:0,
+        top:position
+        })
+        linksContainer.style.height=0
+    })
+})
