86a83bd0bfddac03aee4e766ec421c7b56927aef
Valfredo Felipe de Souza Brito <valfredo155@hotmail.com>
2023-05-26 15:49:41 -0300

    .
    .

--- crud/main.js
+++ crud/main.js
@@ -67,15 +67,28 @@ const saveClient = ()=>{
             email: document.getElementById('email').value,
             celular: document.getElementById('celular').value,
             cidade: document.getElementById('cidade').value
+            
         }
-        createClient(client)
-        clearFields()
-        closeModal()
+        const index =document.getElementById('nome').dataset.index
+        if(index =='new'){
+            createClient(client)
+            clearFields()
+            updateTable()
+            closeModal()
+        
+        } else{
+            updateCLient(index, client)
+            updateTable()
+            closeModal()
+
+        }
+        
+        
         
     }
 }
 
-const createRow = (client) =>{
+const createRow = (client, index) =>{
     const newRow =document.createElement('tr')
     newRow.innerHTML = `
                     <td>${client.nome}</td>
@@ -83,8 +96,8 @@ const createRow = (client) =>{
                     <td>${client.celular}</td>
                     <td>${client.cidade}</td>
                     <td>
-                        <button type="button" class="button green">editar</button>
-                        <button type="button" class="button red">excluir</button>
+                        <button type="button" class="button green" id='edit-${index}'>editar</button>
+                        <button type="button" class="button red" id='delete-${index}'>excluir</button>
                     </td>`
     document.querySelector('#tbody').appendChild(newRow)
 }
@@ -101,6 +114,45 @@ const updateTable = () =>{
     clearTable()
     dbClient.forEach(createRow)
 }
+
+
+const fillFields = (client) =>{
+    document.getElementById('nome').value = client.nome
+    document.getElementById('email').value = client.email
+    document.getElementById('celular').value = client.celular
+    document.getElementById('cidade').value = client.cidade
+    document.getElementById('nome').dataset.index =client.index
+}
+
+const editClient = (index) =>{
+    const client = readClient()[index]
+    client.index=index
+    fillFields(client)
+    openModal()
+}
+
+const editDelete = (evt) => {
+    if(evt.target.type == 'button'){
+
+        const [action,index] = evt.target.id.split('-')
+
+        if(action=='edit'){
+
+         editClient(index)
+
+        }else{
+            const client = readClient()[index]
+            const response = confirm(`Deseja realmente excluir o cliente ${client.nome}`)
+            if(response){
+            deleteClient(index)
+            updateTable()
+            }
+        }
+
+        
+    }
+}
+
 updateTable()
 
 //EVENTOS
@@ -108,4 +160,5 @@ document.getElementById('cadastrarCliente').addEventListener('click', openModal)
 document.getElementById('modalClose').addEventListener('click', closeModal)
 document.getElementById('salvar').addEventListener('click', saveClient)
 document.getElementById('cancelar').addEventListener('click', closeModal)
+document.querySelector('#tableClient>tbody').addEventListener('click', editDelete)
 
